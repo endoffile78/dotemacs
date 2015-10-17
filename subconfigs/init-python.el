867111fe8a995cf1cad7498aa943861bd3f4a3b1
endoffile <endoffile78@yahoo.com>
2015-10-17 07:53:44 -0500

    Made some changes to init-python.el

--- subconfigs/init-python.el
+++ subconfigs/init-python.el
@@ -3,9 +3,12 @@
 (use-package elpy
   :commands elpy-enable
   :init
+  (setq elpy-rpc-backend "jedi")
   (elpy-enable))
 
-(autoload 'jedi:setup "jedi" nil t)
-(add-hook 'python-mode-hook 'jedi:setup)
+(use-package jedi
+  :commands jedi:setup
+  :init
+  (add-hook 'python-mode-hook 'jedi:setup))
 
 (provide 'init-python)
