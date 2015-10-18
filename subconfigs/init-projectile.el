1bcb2528d23dc7a26cb3fbf728e5c1c9f00a7412
endoffile <endoffile78@yahoo.com>
2015-10-18 03:33:16 -0500

    Added in a hook to set the indexing method depeding on if you are in git or not

--- subconfigs/init-projectile.el
+++ subconfigs/init-projectile.el
@@ -1,9 +1,17 @@
 ;;; init-projectile.el --- Projectile settings
 
+(defun my-projectile-hook ()
+  "Checks to see if project is in a git repo or not and then sets the indexing method"
+  (let ((vcs (projectile-project-vcs)))
+    (cond
+     ((eq vcs 'git) (setq projectile-indexing-method 'alien
+						  projectile-enable-caching nil))
+     (t (setq projectile-indexing-method 'native
+  			  projectile-enable-caching t)))))
+
 (use-package projectile
   :config
-  (setq projectile-indexing-method 'native
-		projectile-enable-caching t)
+  (add-hook 'projectile-before-switch-project-hook 'my-projectile-hook)
   (projectile-global-mode))
 
 (provide 'init-projectile)
