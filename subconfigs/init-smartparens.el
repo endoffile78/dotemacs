fbfa36ffe82ee5e52f7bcaab4d1a24d7e84864de
endoffile <endoffile78@yahoo.com>
2015-10-26 03:37:29 -0500

    Use show-smartparens-mode instead of paren-mode

--- subconfigs/init-smartparens.el
+++ subconfigs/init-smartparens.el
@@ -3,7 +3,11 @@
 (use-package smartparens-config
   :diminish smartparens-mode
   :config
+  (require 'smartparens-config)
+  (setq sp-show-pair-delay 0
+		sp-show-pair-from-inside t)
   (sp-use-smartparens-bindings)
+  (show-smartparens-global-mode)
   (smartparens-global-mode t))
 
 (provide 'init-smartparens)
