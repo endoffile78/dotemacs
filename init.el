ef756b20d186858d49353a66325d1a56c2c5f753
endoffile78 <endoffile78@users.noreply.github.com>
2016-03-06 20:30:20 -0600

    Enable flyspell for certain modes and update theme

--- init.el
+++ init.el
@@ -592,6 +592,31 @@ _q_uit
   :mode (("CMakeLists\\.txt\\'" . cmake-mode)
 		 ("\\.cmake\\'" . cmake-mode)))
 
+;; flyspell
+
+(use-package flyspell
+  :init
+  (add-hook 'org-mode-hook 'flyspell-mode)
+  (add-hook 'markdown-mode-hook 'flyspell-mode)
+  (add-hook 'text-mode-hook 'flyspell-mode)
+  :config
+  ;; NO spell check for embedded snippets
+  (defadvice org-mode-flyspell-verify (after org-mode-flyspell-verify-hack activate)
+	(let ((rlt ad-return-value)
+		  (begin-regexp "^[ \t]*#\\+begin_\\(src\\|html\\|latex\\)")
+		  (end-regexp "^[ \t]*#\\+end_\\(src\\|html\\|latex\\)")
+		  old-flag
+		  b e)
+	  (when ad-return-value
+		(save-excursion
+		  (setq old-flag case-fold-search)
+		  (setq case-fold-search t)
+		  (setq b (re-search-backward begin-regexp nil t))
+		  (if b (setq e (re-search-forward end-regexp nil t)))
+		  (setq case-fold-search old-flag))
+		(if (and b e (< (point) e)) (setq rlt nil)))
+	  (setq ad-return-value rlt))))
+
 ;; Misc
 
 (use-package fancy-battery-mode
