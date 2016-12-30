98feb8d5ce93b408e787982c658acf203393a9f8
endoffile78 <endoffile78@yahoo.com>
2016-12-30 11:49:02 -0600

    Remove tern and company-tern

--- init.el
+++ init.el
@@ -561,15 +561,6 @@
   :ensure
   :mode ("\\.js$" . js2-mode))
 
-(use-package tern
-  :commands tern-mode
-  :init
-  (add-hook 'js2-mode-hook (lambda () (tern-mode t))))
-
-(use-package company-tern
-  :config
-  (add-to-list 'company-backends 'company-tern))
-
 ;; Yasnippet
 
 (use-package yasnippet
