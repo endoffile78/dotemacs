34d3bea671c691e355cc93a7f9e51de2624b3b98
endoffile78 <endoffile78@yahoo.com>
2019-08-02 14:39:05 -0500

    Add setup.ps1 for when I need to use emacs on windows

---
+++ setup.ps1
@@ -0,0 +1,5 @@
+if (Get-Command choco -errorAction SilentlyContinue) {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+choco install emacs
